bd647ae6bcf1270d44a039d65cc43e417d444275
McGyver105 <steve.titchener@hotmail.com>
2021-03-03 10:55:43 +0000

    rewrites the place ids around bury

--- db/development_data/reviews.js
+++ db/development_data/reviews.js
@@ -1,32 +1,32 @@
 module.exports = [
   {
     username: "happy400",
-    place_id: 1,
+    place_id: "ChIJr5GU_haje0gROgaokPtEXnk",
     review: "I had an amazing time here. Great owners. Very friendly.",
     created_at: 1511354163389,
   },
   {
     username: "tea10000",
-    place_id: 4,
+    place_id: "ChIJE2PelnO7e0gR-QaVL6WoGiU",
     review:
       "Nice part of the country to stay in. I had such an amazing time here walking around the countryside, breathing all that fresh air, seeing all the birds and animals. You know this reminds me of a time when I was sitting in a park talking to my friend, Orville. Orville,  I said, isnt this a great place to be. But of course Orville couldnt respond as he is an elephant, and elephants cant speak English. But thats not important right now to the story. Because you see sometimes a story is just a story and we shouldnt dwell on these things. Anyway, Orville and I discussed the countryside and had a great time feeding the ducks that day.",
     created_at: 1509818163389,
   },
   {
     username: "printer102",
-    place_id: 3,
+    place_id: "ChIJ74NqxMQGe0gRQHCWU1R6DJk",
     review: "I can only tell you if we meet under a full moon.",
     created_at: 1406746163290,
   },
   {
     username: "duck400",
-    place_id: 2,
+    place_id: "ChIJbXZLN0uje0gRV8Qevk2u7Vo",
     review: "Had a great time here. Really peaceful at night.",
     created_at: 1470046203389,
   },
   {
     username: "duck400",
-    place_id: 1,
+    place_id: "ChIJX2VVFVnOe0gRYz0kjHQIP7U",
     review:
       "Easy to park my caravan here. Really good setup with wide access and room to maneuver.",
     created_at: 1363684164340,
